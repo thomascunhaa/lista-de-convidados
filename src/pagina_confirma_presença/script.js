663390a347ec3b0acc38c26bae01c78063d615e8
Thomas Gomes <thomasgomes_cpti@outlook.com>
2024-04-17 00:16:43 -0300

    add pop up

--- src/pagina_confirma_presença/script.js
+++ src/pagina_confirma_presença/script.js
@@ -4,9 +4,15 @@ const dados_do_formulario = document.forms['confirmarPresencaForm'];
 dados_do_formulario.addEventListener('submit', function(event) {
   event.preventDefault(); // Evita o envio padrão do formulário
 
-  fetch(scrip_do_google, {method: 'POST', body: new FormData(dados_do_formulario)}).then(response => {
-    alert('Dados enviado com sucesso!', response);
-    dados_do_formulario.reset();
-  })
-  .catch(error => console.log('Erro enviar dos dados.'))
+  fetch(scrip_do_google, {method: 'POST', mode: 'no-cors', body: new FormData(dados_do_formulario)})
+.then(response => {
+  if (!response.ok) {
+    window.location.href = 'confirmacao.html';  
+    throw new Error('Erro ao enviar os dados.');
+  }
+  alert('Dados enviados com sucesso!');
+  dados_do_formulario.reset();
+  // Redireciona para outra página HTML
+})
+.catch(error => console.error(error));
 });
